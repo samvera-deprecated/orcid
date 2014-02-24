5f168aeb418dc77b9d0e8c54dddb99eab6ff0646
Jeremy Friesen <jeremy.n.friesen@gmail.com>
2014-02-24 14:30:35 -0500

    Teasing out horribly named ProfileBatchConnector
    This new class is on the chopping block already but highlights some of
    the behavior that I'm looking for in regards to the ProfileConnection.

---
+++ spec/models/orcid/profile_batch_connector_spec.rb
@@ -0,0 +1,42 @@
+require 'spec_helper'
+
+module Orcid
+  describe ProfileBatchConnector do
+    Given(:context) { double(invoked: true) }
+    Given(:query_result) { false }
+    Given(:query_service) { double("Query Service", call: query_result) }
+    Given(:config) { {query_service: query_service } }
+    Given(:runner) {
+      described_class.new(config) { |on|
+        on.found {|results| context.invoked("FOUND", results) }
+        on.not_found { context.invoked("NOT FOUND") }
+      }
+    }
+    Given(:parameters) { { email: 'hello@world.com' } }
+
+    context '.found' do
+      Given(:query_result) { [1,2] }
+      When { runner.call(parameters) }
+      Then {
+        query_service.should(
+          have_received(:call).
+          with(q: "email:#{parameters.fetch(:email)}")
+        )
+      }
+      Then { context.should have_received(:invoked).with("FOUND", query_result)}
+    end
+
+    context '.not_found' do
+      Given(:query_result) { [] }
+      When { runner.call(parameters) }
+      Then {
+        query_service.should(
+          have_received(:call).
+          with(q: "email:#{parameters.fetch(:email)}")
+        )
+      }
+      Then { context.should have_received(:invoked).with("NOT FOUND")}
+    end
+
+  end
+end
