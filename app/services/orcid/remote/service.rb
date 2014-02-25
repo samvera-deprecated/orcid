50d302426e1f71a45ce03ba4a3832fb291c59f44
Jeremy Friesen <jeremy.n.friesen@gmail.com>
2014-02-25 15:53:00 -0500

    Tweaking Remote::ProfileCreationService
    I'm trying to move the callbacks onto the the service as it is the one
    that has the best handle of states.

---
+++ app/services/orcid/remote/service.rb
@@ -0,0 +1,19 @@
+require_dependency './lib/orcid/named_callbacks'
+module Orcid::Remote
+  class Service
+    def initialize
+      @callbacks = Orcid::NamedCallbacks.new
+      yield(@callbacks) if block_given?
+    end
+
+    def call
+      raise NotImplementedError.new("Define #{self.class}#call")
+    end
+
+    def callback(name, *args)
+      @callbacks.call(name, *args)
+      args
+    end
+
+  end
+end
