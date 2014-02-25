892c81c27e03fb0b7680032742912c4c404fd7c4
Jeremy Friesen <jeremy.n.friesen@gmail.com>
2014-02-25 09:56:23 -0500

    Extracting inner class to its own file
    Instead of having the inline untested class, I am instead moving it
    to an separate file with its own test.

---
+++ spec/services/orcid/remote/profile_lookup_service/search_response_spec.rb
@@ -0,0 +1,12 @@
+require 'spec_helper'
+
+module Orcid::Remote
+  describe ProfileLookupService::SearchResponse do
+    Given(:attributes) { {id: 'Hello', label: 'World', junk: 'JUNK!'} }
+    Given(:search_response) { described_class.new(attributes) }
+    Then { expect(search_response.id).to eq(attributes[:id]) }
+    And { expect(search_response.label).to eq(attributes[:label]) }
+    And { expect(search_response.orcid_profile_id).to eq(attributes[:id]) }
+    And { expect{search_response.junk }.to raise_error }
+  end
+end
