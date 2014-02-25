5daecb4af60c5442aaaf090db58fd1b95ada807a
Jeremy Friesen <jeremy.n.friesen@gmail.com>
2014-02-25 15:51:09 -0500

    Adding StubCallback to spec support

---
+++ spec/support/stub_callback.rb
@@ -0,0 +1,25 @@
+class StubCallback
+  def invoke(*args)
+    @invoked_args = args
+  end
+
+  def invoked(*args)
+    @invoked_args
+  end
+
+  def configure(*names)
+    lambda do |on|
+      configure_name(on, :success)
+      configure_name(on, :failure)
+      names.each do |name|
+        configure_name(on, name)
+      end
+    end
+  end
+
+  private
+
+  def configure_name(on, name)
+    on.send(name) { |*args| invoke(name, *args) }
+  end
+end
