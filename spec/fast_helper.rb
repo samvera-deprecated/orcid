7d1f9de6d19d225f9548278a6068830816c07753
Jeremy Friesen <jeremy.n.friesen@gmail.com>
2014-02-25 19:04:59 -0500

    Updating $LOAD_PATH for fast specs

--- spec/fast_helper.rb
+++ spec/fast_helper.rb
@@ -1,7 +1,6 @@
 require 'rspec/given'
-$LOAD_PATH << File.expand_path("../", __FILE__)
+$LOAD_PATH << File.expand_path("../..", __FILE__)
 require 'spec/support/stub_callback'
-
 
 unless defined?(require_dependency)
   def require_dependency(*files)
