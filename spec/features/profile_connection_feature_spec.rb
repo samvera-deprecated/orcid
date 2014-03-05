9b6d2f412acecc9441667881c9b78068a8952e5a
Jeremy Friesen <jeremy.n.friesen@gmail.com>
2014-03-05 09:22:33 -0600

    Removing ProfileConnection ENV variable req

--- spec/features/profile_connection_feature_spec.rb
+++ spec/features/profile_connection_feature_spec.rb
@@ -1,28 +1,19 @@
 require 'spec_helper'
 
 module Orcid
-  if ! ENV['ORCID_EXISTING_PUB_EMAIL']
-    message = "Please set ENV['ORCID_EXISTING_PUB_EMAIL'] to run these specs."
-    Rails.logger.error(message)
-    puts "*"*80 << "\n" << message << "\n" << '*' *80
-  else
-    describe 'profile connection feature', requires_net_connect: true do
-      around(:each) do |example|
-        WebMock.allow_net_connect!
-        example.run
-        WebMock.disable_net_connect!
-      end
-
-      Given(:user) { FactoryGirl.create(:user) }
-      Given(:email) { ENV['ORCID_EXISTING_PUB_EMAIL'] }
-      Given(:profile_connect) { ProfileConnection.new(user: user, email: email) }
-
-      When(:profile_candidates) { profile_connect.orcid_profile_candidates }
-
-      Then { expect(profile_candidates.count).to eq(1) }
-      Then {
-        expect(profile_candidates.first.orcid_profile_id).to eq(ENV['ORCID_EXISTING_PUB_PROFILE_ID'])
-      }
+  describe 'connect to a publicly visible profile', requires_net_connect: true do
+    around(:each) do |example|
+      WebMock.allow_net_connect!
+      example.run
+      WebMock.disable_net_connect!
     end
+
+    Given(:user) { FactoryGirl.create(:user) }
+    Given(:text) { '"Jeremy Friesen"' }
+    Given(:profile_connect) { ProfileConnection.new(user: user, text: text) }
+
+    When(:profile_candidates) { profile_connect.orcid_profile_candidates }
+
+    Then { expect(profile_candidates.count).to be > 1 }
   end
 end
