9092bf5d89ad38efc1a35d221bb28da4a9727a22
Jeremy Friesen <jeremy.n.friesen@gmail.com>
2014-03-05 10:08:50 -0600

    Adding SearchResponse#biography
    I want to expose the biography as well.

--- spec/services/orcid/remote/profile_query_service/search_response_spec.rb
+++ spec/services/orcid/remote/profile_query_service/search_response_spec.rb
@@ -3,9 +3,10 @@ require 'orcid/remote/profile_query_service/search_response'
 
 module Orcid::Remote
   describe ProfileQueryService::SearchResponse do
-    Given(:attributes) { {id: 'Hello', label: 'World', junk: 'JUNK!'} }
+    Given(:attributes) { {id: 'Hello', label: 'World', junk: 'JUNK!', biography: "Extended Biography"} }
     Given(:search_response) { described_class.new(attributes) }
     Then { expect(search_response.id).to eq(attributes[:id]) }
+    And { expect(search_response.biography).to eq(attributes[:biography]) }
     And { expect(search_response.label).to eq(attributes[:label]) }
     And { expect(search_response.orcid_profile_id).to eq(attributes[:id]) }
     And { expect{search_response.junk }.to raise_error }
