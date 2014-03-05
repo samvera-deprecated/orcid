e0482ff7edd7b92a37a09833272efd28b40a3b72
James Halliday <jhallida@indiana.edu>
2014-03-05 10:01:38 -0500

    Workaround for OM bug

--- app/models/orcid/work.rb
+++ app/models/orcid/work.rb
@@ -12,7 +12,6 @@ module Orcid
       "artistic-performance","book-chapter","book-review","book","conference-abstract","conference-paper","conference-poster","data-set","dictionary-entry","disclosure","dissertation","edited-book","encyclopedia-entry","invention","journal-article","journal-issue","lecture-speech","license","magazine-article","manual","newsletter-article","newspaper-article","online-resource","other","patent","registered-copyright","report","research-technique","research-tool","spin-off-company","standards-and-policy","supervised-student-publication","technical-standard","test","translation","trademark","website","working-paper",
     ].freeze
 
-    include Virtus.model
     include ActiveModel::Validations
     extend ActiveModel::Naming
 
@@ -25,19 +24,13 @@ module Orcid
     #attribute :put_code, String
 
     set_terminology do |t|
-      t.root(path: "orcid-message")
-      t.message_version(path: "message-version")
+      t.root(path: "orcid-work")
+      t.title(:path=>'work-title')
     end
 
-    # This method is called when you create new XML documents from scratch.
-    # It must return a Nokogiri::Document.  Other than that, you can make your "default" documents look however you want.
     def self.xml_template
-      Nokogiri::XML.parse("<orcid-message/>")
+      Nokogiri::XML.parse("<orcid-work><work-title></work-title></orcid-work>")
     end
-
-    #def to_xml
-    #  XmlRenderer.call(self)
-    #end
 
     def ==(comparison_object)
       super || comparison_object.instance_of?(self.class) &&
@@ -52,55 +45,6 @@ module Orcid
         [title, work_type]
       else
         nil
-      end
-    end
-
-    class XmlRenderer
-      def self.call(works, options = {})
-        new(works, options).call
-      end
-
-      attr_reader :works, :template
-      def initialize(works, options = {})
-        self.works = works
-        @template = options.fetch(:template_path) { Orcid::Engine.root.join('app/templates/orcid/work.template.v1.1.xml.erb').read }
-      end
-
-      def call
-        ERB.new(template).result(binding)
-      end
-
-      protected
-      def works=(thing)
-        @works = Array(thing)
-      end
-
-    end
-
-    class XmlParser
-      def self.call(xml)
-        new(xml).call
-      end
-
-      attr_reader :xml
-      def initialize(xml)
-        @xml = xml
-      end
-
-      def call
-        document = Nokogiri::XML.parse(xml)
-        document.css('orcid-works orcid-work').collect do |node|
-          transform(node)
-        end
-      end
-
-      private
-      def transform(node)
-        Orcid::Work.new.tap do |work|
-          work.put_code = node.attributes.fetch("put-code").value
-          work.title = node.css('work-title title').text
-          work.work_type = node.css('work-type').text
-        end
       end
     end
   end
