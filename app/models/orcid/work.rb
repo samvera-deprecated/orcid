33e231094ccfb02e87b5ceedbf1cdb86221b3df6
James Halliday <jhallida@indiana.edu>
2014-03-06 16:59:10 -0500

    Most spec tests working now - work XML is spitting out an XML header, which is getting nested inside another XML header, causing problems

--- app/models/orcid/work.rb
+++ app/models/orcid/work.rb
@@ -72,6 +72,33 @@ module Orcid
 
     end
 
+    class XmlParser
+      def self.call(xml)
+        new(xml).call
+      end
+
+      attr_reader :xml
+      def initialize(xml)
+        @xml = xml
+      end
+
+      def call
+        document = Nokogiri::XML.parse(xml)
+        document.css('orcid-works orcid-work').collect do |node|
+          transform(node)
+        end
+      end
+
+      private
+      def transform(node)
+        Orcid::Work.new.tap do |work|
+          work.put_code = node.attributes.fetch("put-code").value
+          work.title = node.css('work-title title').text
+          work.work_type = node.css('work-type').text
+        end
+      end
+    end
+
     set_terminology do |t|
     
      t.root(path: "orcid_work", :xmlns => "http://www.orcid.org/ns/orcid" )
