d0158edd3a6f5769d9be8ab425e4835356539339
Paul Clough <paclough@github>
2014-03-06 10:16:17 -0600

    Add support for namespace (name and url). Add xpath to select attributes in addition to elements.

--- app/models/orcid/xml_scrubber.rb
+++ app/models/orcid/xml_scrubber.rb
@@ -3,29 +3,31 @@ module Orcid
   module XmlScrubber
     
     # Using the xml, loop through the elements and replace the orig_character with new_character
-    def scrub(xml, orig_character, new_character)
+    def scrub(xml, orig_character, new_character, namespace_name, namespace_url)
      
-     #Create a Nokogiri document
-     xml_doc = Nokogiri::XML(xml)
+      #Create a Nokogiri document
+      xml_doc = Nokogiri::XML(xml)
 
-     #get all nodes
-     node_set = xml_doc.xpath("//*")
-     
-     node_set.each do |node|
+      #get all nodes
+      if namespace_name && namespace_url
+        node_set  = xml_doc.xpath( "//#{namespace_name}:*", namespace_name => namespace_url )
+        node_set += xml_doc.xpath( "//#{namespace_name}:*/@*", namespace_name => namespace_url )
+      else
+        node_set  = xml_doc.xpath( "//*" )
+        node_set += xml_doc.xpath( "//*/@*" )
+      end
 
-       # rename the node if it has the character that needs replacing
-       if node.node_name.include? orig_character
-         node.name = node.name.gsub(orig_character, new_character)
-       end
+      node_set.each do |node|
 
-     end
+        # rename the node if it has the character that needs replacing
+        if node.node_name.include? orig_character
+          node.name = node.name.gsub(orig_character, new_character)
+        end
+      end
 
-     xml_doc.to_s
+      xml_doc.to_s
 
-    end
-  
+    end  
   end
-
-
 end
 
