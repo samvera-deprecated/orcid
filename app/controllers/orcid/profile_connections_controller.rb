d50cc13ce333f988cfc2f53adac13a8eae64b4d0
Jeremy Friesen <jeremy.n.friesen@gmail.com>
2014-03-31 09:30:41 -0400

    Re-arranging methods in ProfileConnections

--- app/controllers/orcid/profile_connections_controller.rb
+++ app/controllers/orcid/profile_connections_controller.rb
@@ -5,33 +5,9 @@ module Orcid
 
     def index
       redirecting_because_user_does_not_have_a_connected_orcid_profile ||
-      redirecting_because_user_must_verify_their_connected_profile ||
-      redirecting_because_user_has_verified_their_connected_profile
+        redirecting_because_user_must_verify_their_connected_profile ||
+        redirecting_because_user_has_verified_their_connected_profile
     end
-
-    def redirecting_because_user_does_not_have_a_connected_orcid_profile
-      return false if Orcid.profile_for(current_user)
-      flash[:notice] = I18n.t("orcid.connections.messages.profile_connection_not_found")
-      redirect_to orcid.new_profile_connection_path
-      return true
-    end
-    protected :redirecting_because_user_does_not_have_a_connected_orcid_profile
-
-    def redirecting_because_user_must_verify_their_connected_profile
-      return false unless profile = Orcid.profile_for(current_user)
-      return false if profile.verified_authentication?
-
-      redirect_to user_omniauth_authorize_url("orcid")
-      return true
-    end
-    protected :redirecting_because_user_must_verify_their_connected_profile
-
-    def redirecting_because_user_has_verified_their_connected_profile
-      orcid_profile = Orcid.profile_for(current_user)
-      redirect_to '/', flash: {notice: I18n.t("orcid.connections.messages.verified_profile_connection_exists", orcid_profile_id: orcid_profile.orcid_profile_id)}
-      return true
-    end
-    private :redirecting_because_user_has_verified_their_connected_profile
 
     def new
       return false if redirecting_because_user_already_has_a_connected_orcid_profile
@@ -67,6 +43,30 @@ module Orcid
     def new_profile_connection
       @profile_connection ||= Orcid::ProfileConnection.new(params[:profile_connection])
     end
+
+    def redirecting_because_user_does_not_have_a_connected_orcid_profile
+      return false if Orcid.profile_for(current_user)
+      flash[:notice] = I18n.t("orcid.connections.messages.profile_connection_not_found")
+      redirect_to orcid.new_profile_connection_path
+      return true
+    end
+    protected :redirecting_because_user_does_not_have_a_connected_orcid_profile
+
+    def redirecting_because_user_must_verify_their_connected_profile
+      return false unless profile = Orcid.profile_for(current_user)
+      return false if profile.verified_authentication?
+
+      redirect_to user_omniauth_authorize_url("orcid")
+      return true
+    end
+    protected :redirecting_because_user_must_verify_their_connected_profile
+
+    def redirecting_because_user_has_verified_their_connected_profile
+      orcid_profile = Orcid.profile_for(current_user)
+      redirect_to '/', flash: {notice: I18n.t("orcid.connections.messages.verified_profile_connection_exists", orcid_profile_id: orcid_profile.orcid_profile_id)}
+      return true
+    end
+    protected :redirecting_because_user_has_verified_their_connected_profile
 
   end
 end
