7d0e5948652219cb797b18fbbe76ec5754609d2f
Jeremy Friesen <jeremy.n.friesen@gmail.com>
2014-04-29 22:29:52 -0400

    Updating spec to pass Rubocop criteria.

--- spec/views/orcid/profile_requests/new.html.erb_spec.rb
+++ spec/views/orcid/profile_requests/new.html.erb_spec.rb
@@ -5,14 +5,19 @@ describe 'orcid/profile_requests/new.html.erb' do
   it 'renders a form' do
     view.stub(:profile_request).and_return(profile_request)
     render
-    expect(rendered).to have_tag('form.new_profile_request', with: {method: :post}) do
+    expect(rendered).to(
+      have_tag('form.new_profile_request', with: { method: :post })
+    ) do
       with_tag('fieldset') do
-        with_tag('input', with: {name: 'profile_request[given_names]'})
-        with_tag('input', with: {name: 'profile_request[family_name]'})
-        with_tag('input', with: {name: 'profile_request[primary_email]'})
-        with_tag('input', with: {name: 'profile_request[primary_email_confirmation]'})
+        with_tag('input', with: { name: 'profile_request[given_names]' })
+        with_tag('input', with: { name: 'profile_request[family_name]' })
+        with_tag('input', with: { name: 'profile_request[primary_email]' })
+        with_tag(
+          'input',
+          with: { name: 'profile_request[primary_email_confirmation]' }
+        )
       end
-      with_tag('input', with: {type: 'submit'})
+      with_tag('input', with: { type: 'submit' })
     end
   end
 end
