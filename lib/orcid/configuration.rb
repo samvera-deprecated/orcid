4fb9b5949a5b8da4f0b6eadc9376ccc233758363
Jeremy Friesen <jeremy.n.friesen@gmail.com>
2014-05-01 10:28:37 -0400

    Feading Orcid::Configuration to the hound

--- lib/orcid/configuration.rb
+++ lib/orcid/configuration.rb
@@ -1,20 +1,16 @@
 module Orcid
+  # Responsible for exposing the customization mechanism
   class Configuration
     attr_reader :mapper
     def initialize(options = {})
-      @mapper = options.fetch(:mapper) {
-        require 'mappy'
-        ::Mappy
-      }
-      @provider = options.fetch(:provider) {
-        require 'orcid/configuration/provider'
-        Provider.new
-      }
-      @authentication_model = options.fetch(:authentication_model) {
-        require 'devise-multi_auth'
-        ::Devise::MultiAuth::Authentication
-      }
-      @parent_controller = options.fetch(:parent_controller) { '::ApplicationController' }
+      @mapper = options.fetch(:mapper) { default_mapper }
+      @provider = options.fetch(:provider) { default_provider }
+      @authentication_model = options.fetch(:authentication_model) do
+        default_authenticaton_model
+      end
+      @parent_controller = options.fetch(:parent_controller) do
+        '::ApplicationController'
+      end
     end
 
     attr_accessor :provider
@@ -23,8 +19,29 @@ module Orcid
 
     def register_mapping_to_orcid_work(source_type, legend)
       mapper.configure do |config|
-        config.register(source: source_type, target: 'orcid/work', legend: legend)
+        config.register(
+          source: source_type,
+          target: 'orcid/work',
+          legend: legend
+        )
       end
+    end
+
+    protected
+
+    def default_mapper
+      require 'mappy'
+      ::Mappy
+    end
+
+    def default_provider
+      require 'orcid/configuration/provider'
+      Provider.new
+    end
+
+    def default_authenticaton_model
+      require 'devise-multi_auth'
+      ::Devise::MultiAuth::Authentication
     end
   end
 end
