17cd2993d7007bd3da76b5f7ce878e4a9c410383
Jeremy Friesen <jeremy.n.friesen@gmail.com>
2014-05-01 13:41:39 -0400

    Orcid passing style guide

--- lib/orcid/engine.rb
+++ lib/orcid/engine.rb
@@ -1,19 +1,23 @@
+# The namespace for all things related to Orcid integration
 module Orcid
-  class << self
+  module_function
 
-    # As per an isolated_namespace Rails engine.
-    # But the isolated namespace creates issues.
-    def table_name_prefix
-      "orcid_"
-    end
-
-    # Because I am not using isolate_namespace for Orcid::Engine
-    # I need this for the application router to find the appropriate routes.
-    def use_relative_model_naming?
-      true
-    end
+  # As per an isolated_namespace Rails engine.
+  # But the isolated namespace creates issues.
+  # @api private
+  def table_name_prefix
+    'orcid_'
   end
 
+  # Because I am not using isolate_namespace for Orcid::Engine
+  # I need this for the application router to find the appropriate routes.
+  # @api private
+  def use_relative_model_naming?
+    true
+  end
+
+  # While not an isolated namespace engine
+  # @See http://guides.rubyonrails.org/engines.html
   class Engine < ::Rails::Engine
     engine_name 'orcid'
 
@@ -23,6 +27,5 @@ module Orcid
         #{config.root}/app/services
       )
     end
-
   end
 end
