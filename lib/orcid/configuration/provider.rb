0ca3839fa5aefd4b6e8006a4a2fc91dc186c6363
Jeremy Friesen <jeremy.n.friesen@gmail.com>
2014-05-01 10:33:32 -0400

    Feeding Orcid::Configuration::Provider to hound

--- lib/orcid/configuration/provider.rb
+++ lib/orcid/configuration/provider.rb
@@ -1,6 +1,10 @@
 require 'orcid/exceptions'
 module Orcid
   class Configuration
+    # Responsible for negotiating the retrieval of Orcid provider information.
+    # Especially important given that you have private auth keys.
+    # Also given that you may want to request against the sandbox versus the
+    # production Orcid service.
     class Provider
       attr_reader :store
       def initialize(store = ::ENV)
@@ -9,43 +13,52 @@ module Orcid
 
       attr_writer :authentication_scope
       def authentication_scope
-        @authentication_scope ||= store.fetch('ORCID_APP_AUTHENTICATION_SCOPE') {
-          "/authenticate,/orcid-works/create,/orcid-works/update,/read-public"
-        }
+        @authentication_scope ||=
+        store.fetch('ORCID_APP_AUTHENTICATION_SCOPE') do
+          '/authenticate,/orcid-works/create,/orcid-works/update,/read-public'
+        end
       end
 
       attr_writer :site_url
       def site_url
-        @site_url ||= store.fetch('ORCID_SITE_URL') { "http://api.sandbox.orcid.org" }
+        @site_url ||= store.fetch('ORCID_SITE_URL') do
+          'http://api.sandbox.orcid.org'
+        end
       end
 
       attr_writer :token_url
       def token_url
-        @token_url ||= store.fetch('ORCID_TOKEN_URL') { "https://api.sandbox.orcid.org/oauth/token" }
+        @token_url ||= store.fetch('ORCID_TOKEN_URL') do
+          'https://api.sandbox.orcid.org/oauth/token'
+        end
       end
 
       attr_writer :signin_via_json_url
       def signin_via_json_url
-        @signin_via_json_url ||= store.fetch('ORCID_REMOTE_SIGNIN_URL') { "https://sandbox.orcid.org/signin/auth.json" }
+        @signin_via_json_url ||= store.fetch('ORCID_REMOTE_SIGNIN_URL') do
+          'https://sandbox.orcid.org/signin/auth.json'
+        end
       end
 
       attr_writer :authorize_url
       def authorize_url
-        @authorize_url ||= store.fetch('ORCID_AUTHORIZE_URL') { "https://sandbox.orcid.org/oauth/authorize" }
+        @authorize_url ||= store.fetch('ORCID_AUTHORIZE_URL') do
+          'https://sandbox.orcid.org/oauth/authorize'
+        end
       end
 
       attr_writer :id
       def id
         @id ||= store.fetch('ORCID_APP_ID')
       rescue KeyError
-        raise ConfigurationError.new('ORCID_APP_ID')
+        raise ConfigurationError, 'ORCID_APP_ID'
       end
 
       attr_writer :secret
       def secret
         @secret ||= store.fetch('ORCID_APP_SECRET')
       rescue KeyError
-        raise ConfigurationError.new('ORCID_APP_SECRET')
+        raise ConfigurationError, 'ORCID_APP_SECRET'
       end
     end
   end
