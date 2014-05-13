a1b46e4de6c72e3a2aafd0ec7efc25a7f8db586c
Jeremy Friesen <jeremy.n.friesen@gmail.com>
2014-05-13 14:31:49 -0400

    Adding error handling for "weird" search results
    I don't know if these are truly weird retults, but the resulting
    document from the query says there are 0 results but includes 10 nodes
    that look like they could be results.
    
    The returned JSON document is captured in the newly committed file:
        multiple-responses-without-valid-response.json
    
    Given the potential volatility of the response document from Orcid,
    I've also opted to extract the parser for the response document.
    In doing so, it is easier to unit test parsing the response.
    
    Closes #2
    
    Relates to: ORCID/ORCID-Source#1100

---
+++ app/services/orcid/remote/profile_query_service/response_parser.rb
@@ -0,0 +1,53 @@
+require_dependency 'orcid/remote/profile_query_service'
+module Orcid
+  module Remote
+    class ProfileQueryService
+      class ResponseParser
+
+        # A convenience method to expose entry into the ResponseParser function
+        def self.call(document, collaborators = {})
+          new(collaborators).call(document)
+        end
+
+        attr_reader :response_builder, :logger
+
+        def initialize(collaborators = {})
+          @response_builder = collaborators.fetch(:response_builder) do
+            SearchResponse
+          end
+          @logger = collaborators.fetch(:logger) do
+            Rails.logger
+          end
+        end
+
+        def call(document)
+          json = JSON.parse(document)
+          json.fetch('orcid-search-results').fetch('orcid-search-result')
+          .each_with_object([]) do |result, returning_value|
+            profile = result.fetch('orcid-profile')
+            begin
+              identifier = profile.fetch('orcid-identifier').fetch('path')
+              orcid_bio = profile.fetch('orcid-bio')
+              given_names = orcid_bio.fetch('personal-details').fetch('given-names').fetch('value')
+              family_name = orcid_bio.fetch('personal-details').fetch('family-name').fetch('value')
+              emails = []
+              contact_details = orcid_bio['contact-details']
+              if contact_details
+                emails = (contact_details['email'] || []).map {|email| email.fetch('value') }
+              end
+              label = "#{given_names} #{family_name}"
+              label << ' (' << emails.join(', ') << ')' if emails.any?
+              label << " [ORCID: #{identifier}]"
+              biography = ''
+              biography = orcid_bio['biography']['value'] if orcid_bio['biography']
+              returning_value << response_builder.new('id' => identifier, 'label' => label, 'biography' => biography)
+            rescue KeyError => e
+              logger.warn("Unexpected ORCID JSON Response, part of the response has been ignored.\tException Encountered:#{e.class}\t#{e}")
+            end
+            returning_value
+          end
+        end
+      end
+    end
+  end
+end
