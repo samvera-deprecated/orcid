54c18a65b231daf73ba2e8b113846ddbb28f0003
Jeremy Friesen <jeremy.n.friesen@gmail.com>
2014-05-13 20:35:46 -0400

    Creating Orcid::ProfileStatus
    This is an attempt to consolidate that knowledge.
    
    There are several places in which we want to have access to a user's
    Orcid Profile status, and methods related to those states.
    
    The Orcid Profile Status makes use of callbacks because we do not
    have a uniform object that represents each state for a user and their
    orcid profile.
    
    @TODO - review the controllers that have encapsulated this state and
    have it lean on the Orcid::ProfileStatus handler.

---
+++ app/models/orcid/profile_status.rb
@@ -0,0 +1,67 @@
+module Orcid
+  # ProfileStatus.status
+  # **:authenticated_connection** - User has authenticated against the Orcid
+  #   remote system
+  # **:pending_connection** - User has indicated there is a connection, but has
+  #   not authenticated against the Orcid remote system
+  # **:profile_request_pending** - User has requested a profile be created on
+  #   their behalf
+  # **:unknown** - None of the above
+  class ProfileStatus
+    def self.for(user, collaborators = {}, &block)
+      new(user, collaborators, &block).status
+    end
+
+    attr_reader :user, :profile_finder, :request_finder, :callback_handler
+
+    def initialize(user, collaborators = {})
+      @user = user
+      @profile_finder = collaborators.fetch(:profile_finder) { default_profile_finder }
+      @request_finder = collaborators.fetch(:request_finder) { default_request_finder }
+      @callback_handler = collaborators.fetch(:callback_handler) { default_callback_handler }
+      yield(callback_handler) if block_given?
+    end
+
+    def status
+      return callback(:unknown) if user.nil?
+      profile = profile_finder.call(user)
+      if profile
+        if profile.verified_authentication?
+          return callback(:authenticated_connection, profile)
+        else
+          return callback(:pending_connection, profile)
+        end
+      else
+        request = request_finder.call(user)
+        if request
+          return callback(:profile_request_pending, request)
+        else
+          return callback(:unknown)
+        end
+      end
+      return callback(:unknown)
+    end
+
+    private
+
+    def callback(name, *args)
+      callback_handler.call(name, *args)
+      name
+    end
+
+    def default_callback_handler
+      require 'orcid/named_callbacks'
+      NamedCallbacks.new
+    end
+
+    def default_profile_finder
+      require 'orcid'
+      Orcid.method(:profile_for)
+    end
+
+    def default_request_finder
+      require 'orcid/profile_request'
+      ProfileRequest.method(:find_by_user)
+    end
+  end
+end
