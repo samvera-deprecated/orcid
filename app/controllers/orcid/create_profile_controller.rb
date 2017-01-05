24cbca08da7857e3209185832931cde2a092802b
Lukas Miciunas <miciunla@mail.uc.edu>
2017-01-05 15:56:09 -0500

    Saving at a point where eveything works

--- app/controllers/orcid/create_profile_controller.rb
+++ app/controllers/orcid/create_profile_controller.rb
@@ -4,9 +4,9 @@ module Orcid
 		respond_to :html
 
 		def create
-      byebug
 			uri = URI.parse("https://sandbox.orcid.org/oauth/token") 
-			request = Net::HTTP::Post.new(uri) request["Accept"] = "application/json" 
+			request = Net::HTTP::Post.new(uri) 
+			request["Accept"] = "application/json" 
 			request.set_form_data( "client_id" => "APP-DUCNNE8KZ3DGLI66", 
 									           "client_secret" => "9826e131-91eb-414c-814f-7b414e65e1e4", 
 									           "grant_type" => "authorization_code", 
@@ -15,6 +15,8 @@ module Orcid
 			response = Net::HTTP.start(uri.hostname, uri.port, use_ssl: uri.scheme == "https") do |http| 
 				http.request(request) 
 			end
+
+			redirect_to(user_omniauth_authorize_path(:orcid))
 
 		end
 	end
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,6 @@ Orcid::Engine.routes.draw do
     resource :profile_request, only: [:show, :new, :create, :destroy]
     resources :profile_connections, only: [:new, :create, :index]
 
-    post 'create_orcid', to: 'create_profile#create'
+    get 'create_orcid', to: 'create_profile#create'
   end
 end
