e277b318fad9aa0406a159400de98c05b1265280
Lukas Miciunas <miciunla@mail.uc.edu>
2017-01-30 13:39:56 -0500

    Changes to comply with orcid branding rules
    Alter spec to work with code changes

--- spec/views/orcid/profile_connections/_options_to_connect_orcid_profile.html.erb_spec.rb
+++ spec/views/orcid/profile_connections/_options_to_connect_orcid_profile.html.erb_spec.rb
@@ -11,17 +11,8 @@ describe 'orcid/profile_connections/_options_to_connect_orcid_profile.html.erb',
       have_tag('.options-to-connect-orcid-profile') do
         with_tag(
           'a',
-          with: { href: orcid.new_profile_connection_path(profile_connection: { text: default_search_text }) },
-          text: t('orcid/profile_connection.look_up_your_existing_orcid', scope: 'helpers.label')
+          with: { id: "connect-orcid-link"}
         )
-        with_tag(
-          'a',
-          with: { class: 'orcid-on-demand' },
-        )
-        with_tag('form', with: { method: 'post', action: orcid.profile_connections_path }) do
-          with_tag('label', text: t('orcid/profile_connection.orcid_profile_id', scope: 'helpers.label'))
-          with_tag('input', with: { type: 'text', name: 'profile_connection[orcid_profile_id]' })
-        end
       end
     )
   end
