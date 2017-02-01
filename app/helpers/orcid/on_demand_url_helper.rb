4c496ecbae39934a63d51d82862c17c21c563d98
crowesn <crowesn@gmail.com>
2017-02-01 15:10:50 -0500

    refactor connector helper

--- app/helpers/orcid/on_demand_url_helper.rb
+++ app/helpers/orcid/on_demand_url_helper.rb
@@ -1,5 +1,15 @@
 module Orcid::OnDemandUrlHelper 
   def on_demand_url(user)
-    "#{Orcid.provider.authorize_url}?client_id=#{ENV['ORCID_APP_ID']}&response_type=code&scope=/authenticate&redirect_uri=#{orcid.create_orcid_url}&family_names=#{user.last_name if user.respond_to? :last_name }&given_names=#{user.first_name if user.respond_to? :first_name }&email=#{user.email}"
+    connector_params = {
+      client_id: ENV['ORCID_APP_ID'],
+      response_type: 'code',
+      scope: Orcid.provider.authentication_scope,
+      redirect_uri: orcid.create_orcid_url,
+      family_names: (user.last_name if user.respond_to? :last_name),
+      given_names: (user.first_name if user.respond_to? :first_name),
+      email: user.email
+    }  
+
+    "#{Orcid.provider.authorize_url}?#{connector_params.to_query}"
   end
 end
